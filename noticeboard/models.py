cc93b5ce7052bd3300eaf726cbcffe3c6d3af492
javierclavijo <javierclavijog@gmail.com>
2021-02-12 13:21:52 +0100

    Add project files

---
+++ noticeboard/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Notice(models.Model):
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=40)
+    # author = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('Date published', auto_now_add=True)
+    exp_date = models.DateTimeField('Expiration date')
+    body = models.TextField(max_length=500)
+    hidden = models.BooleanField(default=False)
+    # attachment = models.FileField(blank=True)
---
+++ noticeboard/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+
+from . import models
+
+# Create your views here.
+
+
+class IndexView(generic.ListView):
+    model = models.Notice
+
+# TODO: Expired notes view
